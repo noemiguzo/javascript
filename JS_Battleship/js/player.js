3943f6d70434defd27d8794f2c2b876a87be16ac
NoeliaMelgarejo <noelia.melgarejo@jalasoft.com>
2015-03-07 15:08:06 -0400

    Update player.js

--- JS_Battleship/js/player.js
+++ JS_Battleship/js/player.js
@@ -1,9 +1,15 @@
-// Player class
+/**
+ * Class with attributes and methods for Player
+ * @constructor
+ */
 var Player = function(){
 	this.field = new Field();
-	this.name = window.prompt('Your name?');
-	//this.name = 'Leo';
-	
+	this.name = window.prompt('Enter Your name:');
+    /**
+     * Method that return if the ship is alive for the ¨field¨
+     * that method is evaluate in class field
+     * @returns {the evaluation of function isAnyShipAlive(); }
+     */
 	this.isLooser = function() {
 		return this.field.isAnyShipAlive();
 	};
