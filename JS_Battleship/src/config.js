b7c6649d852d765b74843a7d3eb64cfa164ad105
NoeliaMelgarejo <noelia.melgarejo@jalasoft.com>
2015-03-07 06:15:38 -0400

    Update config.js

--- JS_Battleship/src/config.js
+++ JS_Battleship/src/config.js
@@ -1,3 +1,14 @@
+/**
+ * Values for start the game
+ * @type {{size: number,
+  * nplayers: number,
+  * count: number,
+  * numShipsL1: number,
+  * numShipsL2: number,
+   * numShipsL3: number,
+   * RegExp: string:validation of numbers for user}}
+ */
+
 gSetting = {
   size: 8,
   nplayers: 1,
