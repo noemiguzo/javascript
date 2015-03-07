7061853b8d3cc0df563694890a547b1601e12d29
NoeliaMelgarejo <noelia.melgarejo@jalasoft.com>
2015-03-07 18:23:13 -0400

    Update ship.js

--- JS_Battleship/js/ship.js
+++ JS_Battleship/js/ship.js
@@ -1,11 +1,18 @@
-// Ship class
+/**
+ * Class with attributes and methods for Ship
+ * @param id ship
+ * @param size of ship
+ * @constructor
+ */
 var Ship = function(id,size){
 	this.id = id;
 	this.size =size;
 	this.status = 'ALIVE';
 	this.numShots = 0;
 	this.attemptsDrawn=0;
-	
+    /**
+     * Method that change the status according to num of shots
+      */	
 	this.getShot = function(){
 		this.numShots++;
 		this.status = 'HIT';
