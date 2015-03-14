fc07cc90356e330902890b22dcb195b4e6de80ee
NoeliaMelgarejo <noelia.melgarejo@jalasoft.com>
2015-03-13 22:24:39 -0400

    Update shipSpec.js

--- JS_Battleship/js/spec/shipSpec.js
+++ JS_Battleship/js/spec/shipSpec.js
@@ -1,1 +1,28 @@
 //ship spec
+
+ var n;
+	var field;
+	var size;
+	var ships;
+	size="wrong-size";
+	ships=[1,2,1];
+	
+describe('Field - valid shots scenario: ',function(){
+			
+	describe('Given I have a field', function(){
+		ship = new Ship(size,ships);
+		n=field.size;
+		it('When size value are letters should be invalid',function(){
+		  
+			var validRes0=ship._initShips();
+			expect(validRes0).toBe(false);
+		});
+		
+		it('When size value are letters should be invalid',function(){
+		    ships=[1,2,"Wrong-value"];
+		    ship = new Ship(size,ships);
+		  	var validRes0=ship._initShips();
+			expect(validRes0).toBe(false);
+		});
+	});
+});
