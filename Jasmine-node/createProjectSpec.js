f45f177acc33056f507f14e10d8d2a6dfabbba2e
noemiguzo <noemi4j7g@gmail.com>
2015-03-22 01:53:23 +0100

    start firsby
    frisby practic

---
+++ Jasmine-node/createProjectSpec.js
@@ -0,0 +1,24 @@
+//Frisby test
+var frisby = require('frisby');
+frisby.globalSetup({
+	request:{
+		headers:{
+			'Authorization': 'Basic bm9lbWk0ajdnQGdtYWlsLmNvbToyMDQwMTU4OGou'
+			
+		},			
+		inspectOnFailure: true
+	}
+});
+		
+for (var i=0; i<2; i++){		
+	var now = new Date();
+	var project={  
+			Content: "new project"  + now.getTime() + i
+	};
+	frisby.create('Create project')
+		.post('https://todo.ly/api/projects.json',project, {json: true})
+		.expectStatus(200)	
+		.inspectJSON()	
+		.expectJSON(project)
+	.toss();
+}
