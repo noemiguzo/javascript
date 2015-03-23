30e83a0535e067ff323ba28046e09f6016f56ac0
noemiguzo <noemi4j7g@gmail.com>
2015-03-23 22:37:42 +0100

    update create user
    Currently only the authenticated user can be updated

--- Jasmine-node/userSpec.js
+++ Jasmine-node/userSpec.js
@@ -1,38 +1,21 @@
 var frisby = require('frisby');
 var baseURL= 'https://todo.ly/api';
-frisby.globalSetup({
-	request:{
-		headers:{
-			'Authorization': 'Basic bm9lbWk0ajdnQGdtYWlsLmNvbToyMDQwMTU4OGou'
-		}
-	}
-});
+
 //create a user
+var now = new Date();	
+
 var user={
-    Email: 'myqa201406w@email.com',
-    FullName: 'Joe Blow qa',
+    Email: 'myqa' + now.getTime() +'@email.com',
+    FullName: 'Joe qa',
 	"Password": "pASswoRd"
 };
 var verifyUser={
-    Email: 'myqa201406w@email.com',
-    FullName: 'Joe Blow qa'
+    Email: 'myqa' + now.getTime() + '@email.com',
+    FullName: 'Joe qa'
 };
 frisby.create('Create a new user')
 	.post(baseURL + '/user.json',user, {json: true})
 	.expectStatus(200)
 	.expectJSON(verifyUser)
 	.inspectJSON()
-	.afterJSON(function(json){
-		var newUserId = json.Id;
-		console.log('NEW USER ID:', newUserId);		
-		var updateUserInfo = {
-			Email: "updateBy201405@email.com"
-		};
-		frisby.create('Update project')			
-			.put('https://todo.ly/api/user/547994.json', updateUserInfo, {json: true})
-			.put(baseURL + '/user/' + newUserId + '.json', updateUserInfo, {json: true})
-			.expectStatus(200)
-			.inspectJSON()					
-		.toss();		
-	})
 .toss();
