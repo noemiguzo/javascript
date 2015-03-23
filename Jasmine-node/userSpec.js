d9a6477e10445db17ade06f0b33098cb51541b7d
noemiguzo <Noemi.Guzman@jalasoft.com>
2015-03-23 19:00:10 -0400

    create user
    added create user script

---
+++ Jasmine-node/userSpec.js
@@ -0,0 +1,38 @@
+var frisby = require('frisby');
+var baseURL= 'https://todo.ly/api';
+frisby.globalSetup({
+	request:{
+		headers:{
+			'Authorization': 'Basic bm9lbWk0ajdnQGdtYWlsLmNvbToyMDQwMTU4OGou'
+		}
+	}
+});
+//create a user
+var user={
+    Email: 'myqa201405w@email.com',
+    FullName: 'Joe Blow qa',
+	"Password": "pASswoRd"
+};
+var verifyUser={
+    Email: 'myqa201405w@email.com',
+    FullName: 'Joe Blow qa'
+};
+frisby.create('Create a new user')
+	.post(baseURL + '/user.json',user, {json: true})
+	.expectStatus(200)
+	.expectJSON(verifyUser)
+	.inspectJSON()
+	.afterJSON(function(json){
+		var newUserId = json.Id;
+		console.log('NEW USER ID:', newUserId);		
+		var updateUserInfo = {
+			Email: "updateBy201405@email.com"
+		};
+		frisby.create('Update project')			
+			.put('https://todo.ly/api/user/547994.json', updateUserInfo, {json: true})
+			.put(baseURL + '/user/' + newUserId + '.json', updateUserInfo, {json: true})
+			.expectStatus(200)
+			.inspectJSON()					
+		.toss();		
+	})
+.toss();
